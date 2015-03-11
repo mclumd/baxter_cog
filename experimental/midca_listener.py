79448d1a0c14a31ae8e002b999655d1bda5bd4ad
Matthew Paisner <matthew.paisner@gmail.com>
2015-03-11 13:52:18 -0400

    testing midca listener

---
+++ experimental/midca_listener.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys, time
+import rospy
+import roslib
+
+from object_recognition_msgs.msg import ObjectArray
+
+def object_callback(data):
+	if data.objects:
+		print str(len(data)), "objects found."
+		for object in data.objects:
+			print "type:", object.type
+			print "confidence:", object.confidence
+			if not object.point_clouds:
+				print "no point clouds"
+			else:
+				print str(len(object.point_clouds)), "point clouds."
+				print "size(s):", [str(len(pointCloud)) for pointCloud in object.point_clouds]
+			print "mesh:", object.bounding_mesh
+			print "contours:", object.bounding_contours
+			print "pose:", object.pose
+			
+
+def midca_listener():
+    rospy.init_node('Midca_Node', anonymous=False)
+    rospy.Subscriber("/recognized_object_array", ObjectArray, object_callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        midca_listener()
+    except rospy.ROSInterruptException:
+        pass
