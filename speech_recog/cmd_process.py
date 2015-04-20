c595e3b59fc1e8d28fe58fc47827c7d1c124ca93
Matthew Paisner <matthew.paisner@gmail.com>
2015-04-20 15:20:39 -0400

    pointing demo: added ability to 'forget' old observations

--- speech_recog/cmd_process.py
+++ speech_recog/cmd_process.py
@@ -2,15 +2,30 @@ import rospy
 import os
 from std_msgs.msg import String
 from geometry_msgs.msg import Point
+from geometry_msgs.msg import PointStamped
+
+#will not use position reports received further in the past.
+#To disable this behavior, simply set the value to float('inf')
+CURRENT_POSITION_MAX_LAG = 2.0
+
+#convert max delay time to ROS Duration
+currentPositionMaxDuration = rospy.Duration.from_sec(CURRENT_POSITION_MAX_LAG)
 
 pub = None
 quadPos = None
+lastPosTime = None
+
+def pos_str(pos):
+	if not pos:
+		return "()"
+	else:
+		return "(" + str(pos.x) + ", " + str(pos.y) + ", " + str(pos.z) + ")"
 
 def cmd_callback(data):
-	print "got " + data.data
+	rospy.loginfo("got " + data.data)
 	cmd = str(data.data).strip()
 	if cmd == "hello baxter":
-		print "hello commander"
+		rospy.loginfo("hello commander")
 		os.system("espeak -v en 'hello commander'") #speaker?
 	elif cmd == "point to the quad":
 		'''
@@ -19,33 +34,51 @@ def cmd_callback(data):
 		position has been reported.
 		'''
 		if quadPos:
-			pub.publish(quadPos)
-			print "sending command to point to", str(quadPos)
+			t = rospy.get_rostime()
+			rospy.loginfo(str(t) + "," + str(lastPosTime))
+			if lastPosTime != 0 and t - lastPosTime < currentPositionMaxDuration:
+				pub.publish(quadPos)
+				rospy.loginfo("sending command to point to " + 
+				pos_str(quadPos))
+			else:
+				rospy.loginfo("Last quad position is outdated: recorded at " + 
+				pos_str(quadPos) + str(round((t - lastPosTime).to_sec(), 2))
+				+ " seconds ago")
 		else:
-			print "No known quad location"
+			rospy.loginfo("No known quad location")
 	elif cmd == "turn on the quad":
-		print "I do not know how to start quad"
+		rospy.loginfo("I do not know how to start quad")
 	elif cmd == "raise the quad":
-		print "I do not know how to raise quad"
+		rospy.loginfo("I do not know how to raise quad")
 	elif cmd == "lower the quad":
-		print "I do not know how to lower quad"
+		rospy.loginfo("I do not know how to lower quad")
 	elif cmd == "land the quad":
-		print "I do not know how to land quad"
+		rospy.loginfo("I do not know how to land quad")
 	elif cmd == "turn off the quad":
-		print "I do not know how to turn off quad"
+		rospy.loginfo("I do not know how to turn off quad")
 	else:
-		print "command", cmd, "not understood"
+		rospy.loginfo("command " + cmd + " not understood")
 
 def quad_pos_callback(data):
-	print "setting last known quad position to", data
-	global quadPos
-	quadPos = data
+	t = rospy.get_rostime()
+	p = data.point #convert from PointStamped
+	global quadPos, lastPosTime
+	if not p or p.x == 0 and p.y == 0 and p.z == 0:
+		rospy.loginfo("Received a null location or origin. Erasing previous \
+		quad location.")
+		quadPos = None
+		lastPosTime = t
+	else:		
+		rospy.loginfo("setting last known quad position to " + 
+		pos_str(p) + ". t =" + str(round(t.to_sec(), 2)))
+		quadPos = p
+		lastPosTime = t
 	
 def start_node():
-	rospy.init_node('sphinx_test')
+	rospy.init_node('command_node')
 	#subscribe to channels reporting commands and the quadrotor's position
 	rospy.Subscriber("cmds_received", String, cmd_callback)
-	rospy.Subscriber("/quad_position", Point, quad_pos_callback)
+	rospy.Subscriber("/quad_position", PointStamped, quad_pos_callback)
 	#start a publisher to the channel where pointing cmds are sent
 	global pub
 	pub = rospy.Publisher('/point_cmd', Point, queue_size=10)
--- speech_recog/quad_loc_test.py
+++ speech_recog/quad_loc_test.py
@@ -1,20 +1,76 @@
 from __future__ import print_function
 import time
 import rospy
-from geometry_msgs.msg import Point
-
-rospy.init_node('quad_pos_test')
-
-pub = rospy.Publisher('/quad_position', Point, queue_size=10)
+from geometry_msgs.msg import Point, PointStamped
+from std_msgs.msg import String
 
 points = [Point(x = 5.0, y = 0.0, z = 0.0),
 		  Point(x = 8.0, y = 7.0, z = 2.0),
 		  Point(x = 7.0, y = -4.0, z = -0.5)]
-n = 0
-while n < 20:
-	n += 1
+
+def send_rotating_points(nPoints, delay):
+	rospy.init_node('quad_pos_test1')
+	pub = rospy.Publisher('/quad_position', PointStamped, queue_size=10)
+	
+	n = 0
+	while n < nPoints:
+		n += 1
+		time.sleep(delay)
+		p = points[n % 3]
+		print("Sending point command:", p)
+		pub.publish(PointStamped(point = p))
+	rospy.spin()
+
+def full_pointing_test():
+	'''
+	Sends Baxter points, tells it to point to them. Also tests sending
+	null points and the origin, which should both cancel previous loc
+	messages, and maximum time before a location is 'forgotten'"
+	'''
+	rospy.init_node('quad_pos_test2')
+	pub = rospy.Publisher('/quad_position', PointStamped, queue_size=10)
+	cmdPub = rospy.Publisher('cmds_received', String, queue_size=10)
+
 	time.sleep(2)
-	p = points[n % 3]
-	print("Sending point command:", p)
-	pub.publish(p)
-rospy.spin()
+	p = points[0]
+	print("Sending location", p)
+	pub.publish(PointStamped(point = p))
+	time.sleep(0.5)
+	print("sending point command")
+	cmdPub.publish(String("point to the quad"))
+	time.sleep(1.0)
+	p = points[1]
+	print("Sending location", p)
+	pub.publish(PointStamped(point = p))
+	time.sleep(1.0)
+	print("sending point command")
+	cmdPub.publish(String("point to the quad"))
+	time.sleep(0.5)
+	p = points[2]
+	print("Sending location", p)
+	pub.publish(PointStamped(point = p))
+	time.sleep(3.0)
+	print("sending point command - failure expected due to time elapsed")
+	cmdPub.publish(String("point to the quad"))
+	time.sleep(1.0)
+	p = points[2]
+	print("Sending location", p)
+	pub.publish(PointStamped(point = p))
+	time.sleep(0.5)
+	p  = Point(x = 0.0, y = 0.0, z = 0.0)
+	print("Sending origin to cancel", p)
+	pub.publish(PointStamped(point = p))
+	time.sleep(0.5)
+	print("sending point command - failure expected due to cancel")
+	cmdPub.publish(String("point to the quad"))
+	time.sleep(1.0)
+	rospy.spin()
+	
+	
+
+if __name__ == "__main__":
+	#send_rotating_points(20, 2.0)
+	full_pointing_test()
+	
+	
+	
